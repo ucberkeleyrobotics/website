dbe2e9bbeadeb7f81d8cb864e4a3e3670e6e326f
Apollo Jain <apollojain@gmail.com>
2015-10-08 21:56:00 -0700

    complete site

--- server.py
+++ server.py
@@ -19,4 +19,30 @@ def calendar():
 def join():
 	return render_template("join.html")
 
+@app.route('/projects/')
+def projects():
+	return render_template("/projects/index.html")
+
+@app.route('/related/')
+def related():
+	return render_template("/related/index.html")
+
+@app.route('/projects/<filename>')
+def in_projects(filename):
+	return render_template("/projects/" + filename + ".html")
+
+
+@app.route('/related/labs/')
+def labs():
+	print("in labs")
+	return render_template("/related/labs/index.html")
+
+@app.route('/related/clubs/')
+def clubs():
+	return render_template("/related/clubs/index.html")
+
+@app.route('/related/<club_or_lab>/<filename>')
+def two_folder(club_or_lab, filename):
+	return render_template("/related/" + club_or_lab + "/" + filename + ".html")
+
 app.run(debug = True)
