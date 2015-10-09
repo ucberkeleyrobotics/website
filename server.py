517a155a9bec470b0248696c15d06a4cf3c9960c
Apollo Jain <apollojain@gmail.com>
2015-10-08 17:16:38 -0700

    y u werk?

--- server.py
+++ server.py
@@ -3,12 +3,20 @@ from flask import Flask, flash, redirect, render_template, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
-    # if current_user.get_id():
-    #     return redirect(url_dictionary[current_user.get_id()])
-    # else: 
     return render_template("index.html")
-    # return "HELLO ROBOTS"
 
+@app.route('/about')
+def about():
+	return render_template("about.html")
+
+@app.route('/calendar')
+def calendar():
+	return render_template("calendar.html")
+
+@app.route('/join')
+def join():
+	return render_template("join.html")
 
 app.run(debug = True)
